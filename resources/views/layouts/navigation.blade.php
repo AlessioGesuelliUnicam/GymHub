a3119589aa2f4e033746f2f7d7af3ec07cbfbd01
AlessioGesuelliUnicam <105433120+AlessioGesuelliUnicam@users.noreply.github.com>
2023-06-15 00:39:35 +0200

    fix navigation bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,10 +1,10 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class=" mx-10 px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center pt-0.4">
                     <a href="{{ route('dashboard') }}">
                         <img src="{{ asset('images/logo.png')}}" width="100px">
                     </a>
